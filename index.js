91f9ab6eb454a6d85f62a3ca905ea3d6dcbeac20
liliang <1582880360@qq.com>
2020-08-11 14:30:42 +0800

    1.   Dva 添加。

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
 // import App from './App';
-import App from './src/index1';
+import App from './src/index';
 import 'react-native-gesture-handler';
 
 AppRegistry.registerComponent(appName, () => App);
